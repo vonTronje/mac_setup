e08b35b58e4cdb931c8938a0b00a97fb0072b167
Hagen Mahnke <hagenmahnke@googlemail.com>
2016-12-19 13:35:19 +0100

    Add Docker setup
    Install Docker for Mac, start daemon and log in to registry.codevault.io

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@
 
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # tap brew casks
 brew tap caskroom/cask
+
 # install various programs
 brew cask install mattermost
 brew cask install google-chrome
@@ -11,9 +13,30 @@ brew cask install iterm2
 brew cask install alfred
 brew cask install spectacle
 brew cask install skype
+brew cask install docker
 
 brew install tree
 brew install rbenv
+
+# start docker daemon
+open /Applications/Docker.app
+
+# log in to registry.codevault.io
+echo 'Logging to the docker registry.'
+echo 'If you have 2FA enabled, please use a personal access token for Git over HTTP as the password.'
+echo 'You can generate a personal access token at https://codevault.io/profile/personal_access_tokens'
+docker login registry.codevault.io
+
+# give one more chance for correct login to docker
+if [ $? -ne 0] ; then
+    echo 'Login unsuccessful. Please make sure to use the correct email as username and password.'
+    echo 'Important: If you have 2FA enabled you neeed to use a personal access token as the password. Generate one at: https://codevault.io/profile/personal_access_tokens' 
+    docker login registry.codevault.io
+fi
+
+if [ $? -ne 0] ; then
+    echo 'Login unsuccessful. Aborting attempts to login. Remember to log in to the registry when needed and set the DOCKER_LOGIN and GITLAB_ACCESS_TOKEN environment variables.'
+fi
 
 # install some current ruby versions
 rbenv install 2.3.1
